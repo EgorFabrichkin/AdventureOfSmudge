1ae166f51796638ecc9335a5b8afeeb2298b3a3e
EgorFabrichkin <e.fabrichkin@gmail.com>
2023-02-14 15:43:59 +0100

    Change Color (in progress)

---
+++ Assets/Scripts/GameCore/Colors/ChangeColor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace GameCore.Colors
+{
+    public class ChangeColor
+    {
+        private Palette palette = null!;
+
+        public ChangeColor(SpriteRenderer spriteRenderer)
+        {
+            spriteRenderer.color = GetColor(spriteRenderer.color);
+        }
+
+        private Color GetColor(Color previousColor)
+        {
+            var currentColor = palette.GetRandomColor();
+            
+            while (previousColor == currentColor)
+            {
+                currentColor = palette.GetRandomColor();
+            }
+
+            return currentColor;
+        }
+    }
+}
