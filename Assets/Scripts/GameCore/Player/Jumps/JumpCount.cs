5e115b2f9a28c1d864284262b5207645f6853802
EgorFabrichkin <e.fabrichkin@gmail.com>
2023-02-14 15:42:47 +0100

    Jump Count

---
+++ Assets/Scripts/GameCore/Player/Jumps/JumpCount.cs
@@ -0,0 +1,16 @@
+﻿using UniRx;
+
+namespace GameCore.Player.Jumps
+{
+    public class JumpCount
+    {
+        private readonly IntReactiveProperty maxValue = new(20);
+        public IReadOnlyReactiveProperty<int> MaxValue => maxValue;
+
+        public JumpCount(Player player)
+        {
+            player.jumped.AddListener(()=> maxValue.Value--);
+            player.boosted.AddListener(()=> maxValue.Value = 20);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/JumpCountView.cs
@@ -0,0 +1,20 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+using Utils;
+
+namespace UI
+{
+    public class JumpCountView : MonoBehaviour
+    {
+        [SerializeField] private Text text = null!;
+
+        private void Awake()
+        {
+            text.EnsureNotNull("Text not specified");
+        }
+
+        public void CountView(IReadOnlyReactiveProperty<int> jumpCount) => jumpCount.Subscribe(Render);
+        private void Render(int value) => text.text = $"{value}";
+    }
+}
